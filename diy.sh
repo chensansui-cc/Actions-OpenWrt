84e57e6f7c2119d6a2127076d96aa711bf0a8174
chensansui-cc <69887941+chensansui-cc@users.noreply.github.com>
2026-07-12 02:54:04 +0800

    Update feeds.conf.default for Passwall sources
    Add Passwall source repositories to feeds configuration

--- diy.sh
+++ diy.sh
@@ -8,3 +8,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
